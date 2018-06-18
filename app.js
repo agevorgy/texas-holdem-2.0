4d220dec63450ff91a6e263ef28d85bd8bc3dd7a
Teng <n0343950@libp45p-12154dd.lm.lmig.com>
2018-06-18 14:30:02 -0400

    small fixes

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var dotenv = require('dotenv').config();
+var mongoose = require('mongoose');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-var mongoose = require('mongoose');
+const mongoose_url = process.env.MONGODB;
+
+require('./models/users');
+
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB, {
+mongoose.connect(mongoose_url, (err, db) => {
+    if (err !== null) {
+        console.log(`MongoDB connection error: ${err}`);
+    } 
 });
-mongoose.connection.on('error', function() {
-  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
-  process.exit(1);
-});
-// Load environment variables
-//dotenv.load();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
