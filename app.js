4130d65fce1763c52d51c8db84744202b851187c
Gevorgyan <n0344113@libp45p-29475wl.lm.lmig.com>
2018-06-19 09:56:04 -0400

    removed jade from app.js

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
-var createError = require('http-errors');
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-require('dotenv').config();
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-const mongoose_url = process.env.MONGODB;
-
 require('./models/users');
 
-// Connect to MongoDB
-mongoose.connect(mongoose_url, (err, db) => {
-    if (err !== null) {
-        console.log(`MongoDB connection error: ${err}`);
-    } 
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+const users = mongoose.model('user');
+
+var app = express();
+var port = process.env.PORT || 3005;
+
+
+mongoose.connect(process.env.MONGODB, {
 });
+mongoose.connection.on('error', function() {
+  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+  process.exit(1);
+});
+const API = '/texas';
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get(API, (req, res) => res.status(200).send('Hello world!'));
+app.set('view engine', 'html');
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 
 // catch 404 and forward to error handler
@@ -49,7 +55,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json('error');
 });
 
 module.exports = app;
