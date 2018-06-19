260494b576cb15eb87f8a7884d75aba0a454ffe1
Gevorgyan <n0344113@libp45p-29475wl.lm.lmig.com>
2018-06-19 10:33:05 -0400

    app.js

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ var mongoose = require('mongoose');
 //require('./models/users');
 require('./models/session');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
 //const Users = mongoose.model('User');
 const Session = mongoose.model('Session');
 
@@ -23,24 +20,13 @@ mongoose.connection.on('error', function() {
   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
   process.exit(1);
 });
-const API = '/texas';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.get(API, (req, res) => res.status(200).send('Hello world!'));
+app.get('/', (req, res) => res.status(200).send('Hello world!'));
 app.set('view engine', 'html');
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 // catch 404 and forward to error handler
