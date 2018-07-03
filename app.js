e3fc67615da87facedba7cbb013199c8a72db814
Victoria <vteng0406@gmail.com>
2018-07-03 11:16:36 -0400

    hook up join/create session

--- app.js
+++ app.js
@@ -44,9 +44,7 @@ app.get('/getUsers', function (req, res) {
   });
 })
 
-// Returns session ID and user ID (client wants both)
 app.post('/api/create-session', function (req, res) {
-
   var newUser = new User({
     name: req.body.name
   })
@@ -128,20 +126,16 @@ app.put('/api/join-session/:id', function (req, res) {
   })
 })
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
 });
-
 
 module.exports = app;
