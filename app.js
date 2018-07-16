9adad3c57764135c4ec70afafc14b609e4a4452a
Victoria <vteng0406@gmail.com>
2018-07-16 15:44:07 -0400

    configure join session web sockets

--- app.js
+++ app.js
@@ -4,29 +4,26 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors');
-require('./models/schemas');
-
-const Session = mongoose.model('Session');
-const User = mongoose.model('User');
+var _ = require('lodash');
 
 var app = express();
 var port = process.env.PORT || 3005;
 var database = process.env.DATABASE;
 
+var routes = require('./routes');
+
 var server = app.listen(port);
 var io = require('socket.io').listen(server);
 
-io.on('connection', (socket) => {
-  console.log('Connected');
-  socket.on('join', function(data) {
-    console.log(data);
-  })
-})
+require('./models/schemas');
+const Session = mongoose.model('Session');
+const User = mongoose.model('User');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 
+// Handling CORS
+app.use(cors());
 app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
     res.send(200);
@@ -36,8 +33,10 @@ app.use((req, res, next) => {
   }
 });
 
-app.get('/', (req, res) => res.status(200).send('Hello world!'));
 app.set('view engine', 'html');
+
+app.get('/', (req, res) => res.status(200).send('Hello world!'));
+app.use('/', routes);
 
 mongoose.connect(database);
 var db = mongoose.connection;
@@ -45,106 +44,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('we are connected to mongoDB!');
 });
-
-app.get('/getUsers', function (req, res) {
-  Session.find({}, function (err, sessions) {
-    if (err) console.error(`Error getting users: ${err}`);
-
-    return res.send(sessions);
-  });
-});
-
-app.post('/api/create-session', function (req, res) {
-  var newUser = new User({
-    name: req.body.name
-  })
-
-  newUser.save(function (err) {
-    if (err) console.error(`Error creating new user: ${err}`);
-  })
-
-  var users = [];
-  users.push(newUser);
-
-  var newSession = new Session({
-    users: users
-  })
-
-  newSession.save(function (err) {
-    if (err) console.error(`Error creating new session: ${err}`);
-
-    return res.json({
-      _sessionId: newSession.id,
-      _userId: newUser._id
-    });
-  })
-})
-
-// Returns positive int if user exists
-function userExists(userArray, id) {
-  for (var i = 0; i < userArray.length; i++) {
-    if (userArray[i].id == id) {
-      return i;
-    }
-  }
-  return -1;
-}
-
-app.put('/api/join-session/:id', function (req, res) {
-  var sessionId = req.params.id;
-  var userRole = req.body.role;
-  var name = req.body.name;
-  var userId = req.body.userId;
-
-  Session.findOne({ id: sessionId }, function (err, sessions) {
-    if (err) console.error();
-
-    User.find({}, function (err, users) {
-      if (err) console.error();
-
-      // if user Id exists already (moderator), just update user role in BOTH user and session models
-      if ((i = userExists(users, userId)) != -1) {
-
-        users[i].role = userRole;
-        sessions.users[userExists(sessions.users, userId)].role = userRole;
-
-        sessions.save((err) => {
-          if (err) console.error(`Error updating user role in session: ${err}`);
-        }) 
-        
-        users[i].save((err) => {
-          if (err) console.error(`Error updating user role: ${err}`);
-
-          return res.json({ _userId: userId })
-        })
-
-        // create new user and push user to session users
-      } else {
-        var newUser = new User({
-          name: name,
-          role: userRole
-        })
-
-        newUser.save(function (err) {
-          if (err) console.error(`Error adding new user: ${err}`);
-        })
-
-        sessions.users.push(newUser);
-
-        sessions.save(function (err) {
-          if (err) console.error(`Error adding new user to session: ${err}`);
-
-          return res.json({ _userId: newUser._id });
-        });
-      }
-    })
-  })
-})
-
-server.listen(3005, function () {
-  console.log('listening on 3005')
-});
-io.listen(server);
 
 app.use(function (req, res, next) {
   next(createError(404));
@@ -157,5 +56,109 @@ app.use(function (err, req, res, next) {
 
   res.status(err.status || 500);
 });
+
+// Returns positive int if user exists
+function userExists(userArray, id) {
+  for (var i = 0; i < userArray.length; i++) {
+    if (userArray[i].id == id) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// Keeping track of users for web sockets
+let allUsers = {};
+
+// Sockets
+io.on('connection', (socket) => {
+
+  // Join session
+  socket.on('join', (user) => {
+    let currUser = {};
+    let curr = {};
+
+    Session.findOne( {id: user.sessionId}, (err, sessions) => {
+      if (err) console.error(`Error joining session ${user.sessionId}: ${err}`);
+
+      User.find({}, function (err, users) {
+        if (err) console.error();
+  
+        // if user Id exists already (moderator), just update user role in BOTH user and session models
+        if ((i = userExists(users, user.userId)) != -1) {
+  
+          users[i].role = user.role;
+          sessions.users[userExists(sessions.users, user.userId)].role = user.role;
+          
+          sessions.save((err) => {
+            if (err) console.error(`Error updating user role in session: ${err}`);
+          }) 
+  
+          users[i].save((err) => {
+            if (err) console.error(`Error updating user role: ${err}`);
+          })
+
+          currUser = {"moderator": true, "name": user.name, "role": user.role};
+          curr[user.userId] = currUser;
+
+          if (user.sessionId in allUsers) {
+            _.merge(allUsers[user.sessionId], curr);
+          } else {
+            allUsers[user.sessionId] = curr;   
+          }
+
+          socket.emit('user-joined', allUsers[user.sessionId]);
+  
+          // create new user and push user to session users
+        } else {
+          var newUser = new User({
+            name: user.name,
+            role: user.role
+          });
+  
+          newUser.save(function (err) {
+            if (err) console.error(`Error adding new user: ${err}`);
+          })
+  
+          sessions.users.push(newUser);
+  
+          sessions.save(function (err) {
+            if (err) console.error(`Error adding new user to session: ${err}`);
+          });
+
+          currUser = {"moderator": false, "name": user.name, "role": user.role};
+          curr[newUser._id] = currUser;
+
+          if (user.sessionId in allUsers) {
+            _.merge(allUsers[user.sessionId], curr);
+          } else {
+            allUsers[user.sessionId] = curr;   
+          }
+
+          // Update current player
+          socket.emit('user-joined', allUsers[user.sessionId]);
+          // Update all players
+          socket.broadcast.emit('user-joined', allUsers[user.sessionId]);
+        }
+      })
+    }) 
+  });
+
+  socket.on('leave', (data) => {
+    // remove from user database
+    User.deleteOne({ _id: data.userId }, (err) => {
+      if (err) console.error(err);
+    });
+    // remove from session
+    // Session.findOne( { id: data.sessionId }, (err, sessions) => {
+
+    // })
+
+  })
+  
+  socket.on('disconnect', function () {
+    console.log('client disconnect...')
+  })
+})
 
 module.exports = app;
